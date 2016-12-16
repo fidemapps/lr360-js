47b3cfb5d9e86b25097fe6997731eb4ae5b42161
Sebastien Guimont <info@sebastienguimont.com>
2016-12-16 13:22:16 -0500

    Remove parameters validations, now we can identifyMember without external_id.

--- src/client.methods/identify.member.js
+++ src/client.methods/identify.member.js
@@ -3,15 +3,10 @@ import Helper from '../helper/helper';
 
 const METHOD = 'POST';
 const PATH = '/api/gamification/actions/identify-member';
-const ERROR_MESSAGE = 'You must provide either a member ID or an external ID.';
 
 export default function (options, callback) {
 
   options = options || {};
-
-  if (!Helper.hasOneOfRequiredProperties(['member_id', 'external_id'], options)) {
-    this.handleError(ERROR_MESSAGE, callback);
-  }
 
   // gets member id returned and sets it in the client context
   let callbackInterceptor = (error, response) => {
